5775ed0ea85c722477d5de97d4815c5462b42019
bob85670 <bob85670@connect.hku.hk>
2025-06-17 12:23:42 +0800

    finish 2022 Part 1a

--- 2022/A3_P1_1a.py
+++ 2022/A3_P1_1a.py
@@ -1,7 +1,7 @@
 import utils
 import collections
 from collections import defaultdict
-import heapq
+from collections import deque
 
 class Solution(object):
 
@@ -11,90 +11,31 @@ class Solution(object):
         :rtype: int
         """
 
-        m, n = len(grid), len(grid[0]) # don't remove this line
-        # Please code below
-        print("m: ", m, " n: ", n)
-        #queue = collections.deque(['A', 'B', 'C', 'D'])
-        #print(queue.pop())
+        m, n = len(grid), len(grid[0])
+        if grid[0][0] == 1 or grid[m - 1][n - 1] == 1:
+            return -1
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        queue = deque([(0, 0, 0)])
+        # Visited set to avoid cycles
+        visited = {(0, 0)}
 
-        if len(grid) == 1 and len(grid) == 1:
-            return 0
+        while queue:
+            r, c, steps = queue.popleft()
+            if r == m-1 and c == n-1:
+                return steps
 
-        Record = {}
+            for dr, dc in directions:
+                new_r, new_c = r + dr, c + dc
+                if (0 <= new_r < m and 
+                    0 <= new_c < n and 
+                    grid[new_r][new_c] == 0 and 
+                    (new_r, new_c) not in visited
+                ):
+                    queue.append((new_r, new_c, steps + 1))
+                    visited.add((new_r, new_c))
+        return -1
 
-        #i: row   j: column
-        for i in range(m):
-            for j in range(n):
-
-                currentNode = (i, j)
-                print(currentNode)
-                immedList = []
-                originalList = []
-                
-                def findNeighbour(cn1, cn2, m, n):
-                    neigbourList = []
-                    if cn1+1 != m:
-                        neigbourList.append((cn1+1, cn2))
-
-                    if cn2+1 != n:
-                        neigbourList.append((cn1, cn2+1))
-
-                    if cn1-1 != -1:
-                        neigbourList.append((cn1-1, cn2))
-
-                    if cn2-1 != -1:
-                        neigbourList.append((cn1, cn2-1))
-
-                    return neigbourList
-                
-                originalList = findNeighbour(i, j, m, n)
-                print("originalList: ", originalList)
-
-                for k in range(len(originalList)):
-                    if grid[originalList[k][0]][originalList[k][1]] == 1:
-                        continue
-                    else:
-                        immedList.append(originalList[k])
-
-                print("immedList: ", immedList)
-
-                Record[currentNode] = immedList
-
-        print(Record)
-
-        def dfs_board(Record, startNode, endNode):
-            frontier = collections.deque()
-            visited_nodes = []
-            path = []
-            count = -1
-            frontier.append(startNode)
-            print("Initial frontier: ", list(frontier))
-            #print(frontier)
-
-            while frontier:
-                currentNode = frontier.pop()
-
-                if currentNode == endNode:
-                    return count
-
-                if not currentNode in visited_nodes:
-                    print("Exploring: ", currentNode, "...")
-                    visited_nodes.append(currentNode)
-                    count += 1
-
-                    if len(Record[currentNode]) == 0:
-                        count = -1
-                        return count
-
-                    for node in Record[currentNode]:
-                        print("Its child nodes: ", Record[currentNode])
-                        frontier.append(node)
-                        print("frontier: ", list(frontier))
-
-        ans = dfs_board(Record, (0, 0), (m-1, n-1))
-        return ans
-
-
+        
 
 
 
